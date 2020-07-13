39fcac84b8c15d5eac5b055c88fd3866d29da42d
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-07-13 14:30:45 +0300

    fix(pre-commit): fix various typescript issues for it to compile

--- ci/src/preCommit.ts
+++ ci/src/preCommit.ts
@@ -7,14 +7,18 @@ interface Props {
   version: string;
 }
 
+interface Body {
+  runs: { image: string }
+}
+
 export function preCommit(props: Props) {
-  const actionYmlPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action.yml');
+  const actionYmlPath = path.resolve(process.env.GITHUB_WORKSPACE!, 'action.yml');
 
   const fileContent = fs.readFileSync(actionYmlPath, 'utf-8');
-  const content = yaml.parse(fileContent);
+  const content = yaml.parse(fileContent) as Body;
 
   const { image: oldImage } = content.runs;
-  const tag = oldImage.split(':').pop();
+  const tag = oldImage.split(':').pop()!;
   const newImage = oldImage.replace(tag, props.tag);
 
   const newYmlContent = yaml.stringify({
